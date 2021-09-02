93b7a44637d145ef4b4c82754510e2923567afab
easleyclaymore <myears@qq.com>
2021-09-02 12:40:32 +0800

    fix bug

--- source/conf.py
+++ source/conf.py
@@ -61,4 +61,4 @@ html_static_path = ['_static']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = zh_CN
