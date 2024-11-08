794af43e33c6472cbea7bf536082060b6b615e3f
easley <40049427+easleyclaymore@users.noreply.github.com>
2024-11-08 08:32:58 +0000

    from codespace

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# -- Project information -----------------------------------------------------
+# https://sphinx-doc.cn/en/master/usage/configuration.html#project-information
+
+project = '我的游戏帝国'
+copyright = '2000-2092, The Test Project Authors'
+author = 'easleyclaymore'
+version = release = '4.16'
+
+# -- General configuration ------------------------------------------------
+# https://sphinx-doc.cn/en/master/usage/configuration.html#general-configuration
+
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+]
+extensions = []
+language = 'zh-CN'
+master_doc = 'index'
+pygments_style = 'sphinx'
+source_suffix = '.rst'
+#templates_path = ['_templates']
+
+# -- Options for HTML output ----------------------------------------------
+# https://sphinx-doc.cn/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+#html_static_path = ['_static']
